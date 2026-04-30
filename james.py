c0eec08fda6d7a486a112d422313c69df40d668f
Xyleneuk <jamesm0jfp@gmail.com>
2026-04-30 11:42:19 +0100

    Working AI QSO operator - two successful contacts logged

--- james.py
+++
@@ -1,8 +0,0 @@
-from gui.setup_wizard import load_config, save_config
-c = load_config()
-print('Current input_device:', c.get('input_device'))
-print('Current output_device:', c.get('output_device'))
-c['input_device'] = 1
-c['output_device'] = 7
-save_config(c)
-print('Fixed - input=1 output=7')
